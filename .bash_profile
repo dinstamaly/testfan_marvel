667c7879a9ce2d0365bc15adbc3a9a10c815876b
dinstamaly <dinstamaly@gmail.com>
2019-09-02 04:17:46 +0600

    update settingd

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export SECRET_KEY="76ca93bc25cb9b112af10128c0c58410c30c8c739cbecd90"
+export DEBUG_VALUE="TRUE"
--- quiz_test/settings.py
+++ quiz_test/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5dy_=#nt6@@jw+oyvy5i#ir%a(+f)yp)tk13_i53&!^4t9s)+b'
+# SECRET_KEY = '5dy_=#nt6@@jw+oyvy5i#ir%a(+f)yp)tk13_i53&!^4t9s)+b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testfanapp.herokuapp.com']
 
 
 # Application definition
